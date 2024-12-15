c60bf6cc888373927e06a9a55f627067d6003ef0
vicbentu <vicbentu.victor@gmail.com>
2024-12-15 12:22:00 +0100

    Inference upgraded

--- inference.py
+++ inference.py
@@ -14,8 +14,14 @@ openMapPredictor = OpenTransportKNN()
 # make a loop that reads console input
 while True:
     try:
-        lat = float(input("Enter latitude: "))
-        lon = float(input("Enter longitude: "))
+        # lat, lon = map(float, input("Enter latitude and longitude separated by a space: ").split())
+        # Separated by space or coma
+        # lat, lon = map(float, input("Enter latitude and longitude separated by a space: ").split())
+        inputed = input("Enter latitude and longitude separated by a space or comma: ")
+        if ',' in inputed:
+            lat, lon = map(float, inputed.split(','))
+        else:
+            lat, lon = map(float, inputed.split())
         date = input("Enter date (YYYY-MM-DD HH:MM): ")
         
         my_input = preprocess((date, lat, lon), openMapPredictor)
